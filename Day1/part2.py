814d9b3dc2776a0884f3c0d3ff70d8e163ee83d7
Antonio Gauta <antoniogauta@hotmail.com>
2021-12-03 20:26:58 +0100

    Day 1 solution

---
+++ Day1/part2.py
@@ -0,0 +1,9 @@
+input = r"C:\Users\Private\Desktop\AoC2021\Day1\input.txt"
+
+numbers = []
+
+with open(input, "r") as file:
+    for line in file:
+        numbers.append(int(line))
+
+print(sum(x < y for x, y in zip(numbers, numbers[3:])))
