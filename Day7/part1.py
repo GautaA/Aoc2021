cfe27c707bf6132664e38c33035352636374b2d4
agauta <ag006907395@fer.unizg.hr>
2021-12-13 22:42:40 +0100

    Day 7 solution

---
+++ Day7/part1.py
@@ -0,0 +1,21 @@
+import sys
+
+input = r"C:\Users\Toni\Desktop\Aoc2021\Day7\input.txt"
+
+numbers = []
+
+with open(input, "r") as file:
+    for line in file:
+        numbers = [int(x) for x in line.split(",")]
+
+min = sys.maxsize
+
+for x in range(0, max(numbers) + 1):
+    current = 0
+    for y in numbers:
+        current += abs(x - y)
+    if current < min:
+        min = current
+
+print(min)
+
